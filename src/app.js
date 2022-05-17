1d1332ecba3f5d9ed71a3431ce6004c1f5211463
William Hagan <whagan6@gatech.edu>
2022-05-17 10:19:54 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        HaganLife Master
+      </header>
+    </div>
+  );
+}
+
+export default App;
