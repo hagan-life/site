c0b8bb7d4100e7ff807a654b915b0fd4d51d7d2c
William Hagan <whagan6@gatech.edu>
2022-05-18 13:14:56 -0500

    deploying via ci/cd -- .htaccess still needs work

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ function App() {
       <header className="app-header">
         HaganLife 20220518@12:55
       </header>
+      <p>need to update apple-touch-icon /logo192.png</p>
     </div>
   );
 }
